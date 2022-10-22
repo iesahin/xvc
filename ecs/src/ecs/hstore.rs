14bbfb3d0ca319ddeb2588597a584889ac31c136
Emre Sahin <github@emresult.com>
2022-10-22 12:37:08 +0300

    removed bstore conversion functions

--- ecs/src/ecs/hstore.rs
+++ ecs/src/ecs/hstore.rs
@@ -14,7 +14,6 @@ use std::fmt::Debug;
 use std::hash::Hash;
 use std::iter::{FromIterator, Iterator};
 
-use super::bstore::BStore;
 use super::vstore::VStore;
 
 use std::ops::{Deref, DerefMut};
@@ -46,21 +45,6 @@ impl<T> From<HashMap<XvcEntity, T>> for HStore<T> {
     }
 }
 
-impl<T> From<BStore<T>> for HStore<T>
-where
-    T: Storable,
-{
-    fn from(store: BStore<T>) -> Self {
-        match store.to_hstore() {
-            Ok(hs) => hs,
-            Err(_) => {
-                Error::StoreConversionError.error();
-                Self::new()
-            }
-        }
-    }
-}
-
 impl<T> FromIterator<(XvcEntity, T)> for HStore<T> {
     fn from_iter<I: IntoIterator<Item = (XvcEntity, T)>>(iter: I) -> Self {
         Self {
@@ -87,15 +71,6 @@ impl<T> HStore<T>
 where
     T: Storable,
 {
-    /// Convert to [BStore]
-    pub fn to_bstore(&self) -> Result<BStore<T>> {
-        let mut store = BStore::new();
-        for (k, v) in self.map.iter() {
-            store.insert(*k, v.clone());
-        }
-        Ok(store)
-    }
-
     /// Convert to [VStore]
     pub fn to_vstore(&self) -> Result<VStore<T>> {
         let mut store = VStore::new();
