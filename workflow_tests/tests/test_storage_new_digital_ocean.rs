7f1ca5b6b9910a0760443b97546d367e700c379f
Emre Sahin <github@emresult.com>
2022-10-30 19:39:35 +0300

    updated to use config file for s3cmd

--- workflow_tests/tests/test_storage_new_digital_ocean.rs
+++ workflow_tests/tests/test_storage_new_digital_ocean.rs
@@ -12,6 +12,108 @@ use xvc_core::XvcRoot;
 use xvc_storage::storage::XVC_STORAGE_GUID_FILENAME;
 use xvc_test_helper::{create_directory_tree, generate_filled_file};
 
+fn write_s3cmd_config(region: &str, access_key: &str, secret_key: &str) -> Result<String> {
+    let config_file_name = env::temp_dir().join(format!(
+        "{}.cfg",
+        common::random_dir_name("wasabi-config", None)
+    ));
+    let config = format!(
+        r#"[default]
+access_key = {access_key}
+access_token =
+add_encoding_exts =
+add_headers =
+bucket_location = eu-central-1
+ca_certs_file =
+cache_file =
+check_ssl_certificate = True
+check_ssl_hostname = True
+cloudfront_host = cloudfront.amazonaws.com
+connection_max_age = 5
+connection_pooling = True
+content_disposition =
+content_type =
+default_mime_type = binary/octet-stream
+delay_updates = False
+delete_after = False
+delete_after_fetch = False
+delete_removed = False
+dry_run = False
+enable_multipart = True
+encoding = UTF-8
+encrypt = False
+expiry_date =
+expiry_days =
+expiry_prefix =
+follow_symlinks = False
+force = False
+get_continue = False
+gpg_command = None
+gpg_decrypt = %(gpg_command)s -d --verbose --no-use-agent --batch --yes --passphrase-fd %(passphrase_fd)s -o %(output_file)s %(input_file)s
+gpg_encrypt = %(gpg_command)s -c --verbose --no-use-agent --batch --yes --passphrase-fd %(passphrase_fd)s -o %(output_file)s %(input_file)s
+gpg_passphrase =
+guess_mime_type = True
+host_base = {region}.digitaloceanspaces.com
+host_bucket = %(bucket)s.{region}.digitaloceanspaces.com
+human_readable_sizes = False
+invalidate_default_index_on_cf = False
+invalidate_default_index_root_on_cf = True
+invalidate_on_cf = False
+kms_key =
+limit = -1
+limitrate = 0
+list_allow_unordered = False
+list_md5 = False
+log_target_prefix =
+long_listing = False
+max_delete = -1
+mime_type =
+multipart_chunk_size_mb = 15
+multipart_copy_chunk_size_mb = 1024
+multipart_max_chunks = 10000
+preserve_attrs = True
+progress_meter = True
+proxy_host =
+proxy_port = 0
+public_url_use_https = False
+put_continue = False
+recursive = False
+recv_chunk = 65536
+reduced_redundancy = False
+requester_pays = False
+restore_days = 1
+restore_priority = Standard
+secret_key = {secret_key}
+send_chunk = 65536
+server_side_encryption = False
+signature_v2 = False
+signurl_use_https = False
+simpledb_host = sdb.amazonaws.com
+skip_existing = False
+socket_timeout = 300
+ssl_client_cert_file =
+ssl_client_key_file =
+stats = False
+stop_on_error = False
+storage_class =
+throttle_max = 100
+upload_id =
+urlencoding_mode = normal
+use_http_expect = False
+use_https = True
+use_mime_magic = True
+verbosity = WARNING
+website_endpoint = http://%(bucket)s.s3-website-%(location)s.amazonaws.com/
+website_error =
+website_index = index.html
+"#
+    );
+
+    fs::write(&config_file_name, config)?;
+
+    Ok(config_file_name.to_string_lossy().to_string())
+}
+
 fn create_directory_hierarchy() -> Result<XvcRoot> {
     let temp_dir: XvcRoot = run_in_temp_xvc_dir()?;
     // for checking the content hash
@@ -41,10 +143,10 @@ fn test_storage_new_digital_ocean() -> Result<()> {
     let secret_key = env::var("DIGITAL_OCEAN_SECRET_ACCESS_KEY")?;
     let region = "fra1";
 
+    let config_file_name = write_s3cmd_config(region, &access_key, &secret_key)?;
+
     let s3cmd = |cmd: &str, append: &str| -> String {
-        let acc = access_key.clone();
-        let sec = secret_key.clone();
-        let sh_cmd = format!("s3cmd {cmd} --host={region}.digitaloceanspaces.com --access_key={acc} --secret_key={sec} {append}");
+        let sh_cmd = format!("s3cmd --config {config_file_name} {cmd} {append}");
         sh(sh_cmd)
     };
 
