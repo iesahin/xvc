15977751ec9c44978ca2eb9f7ebcfa3ed775c8dc
Emre Sahin <github@emresult.com>
2022-11-01 09:59:00 +0300

    added more documentation

--- logging/src/lib.rs
+++ logging/src/lib.rs
@@ -1,3 +1,7 @@
+//! Xvc logging and output crate to be used in output channels.
+//! Xvc uses to discriminate outputs of various types, (Info, Debug, Error...) and can use
+//! `crossbeam_channel` to send these separately.
+//! Downstream crates (xvc, xvc-file, etc.) use this crate not to use stdout, stderr directly.
 #![warn(missing_docs)]
 #![forbid(unsafe_code)]
 use log::LevelFilter;
@@ -6,6 +10,7 @@ use std::env;
 use std::path::Path;
 use std::sync::Once;
 
+/// Debugging macro to print the given expression and its value, with the module, function and line number
 #[macro_export]
 macro_rules! watch {
     ( $( $x:expr ),* ) => {
@@ -17,20 +22,17 @@ macro_rules! watch {
     };
 }
 
-// Logging
-
+/// Logging Initializer
 static INIT: Once = Once::new();
 
+/// Init logging if it's not initialized before.
+/// Uses [Once] to run (non-public fn) `init_logging` once.
 pub fn setup_logging(term_level: Option<LevelFilter>, file_level: Option<LevelFilter>) {
     INIT.call_once(|| init_logging(term_level, file_level));
 }
 
 fn init_logging(term_level: Option<LevelFilter>, file_level: Option<LevelFilter>) {
-    let logfilename = &format!(
-        "{}/xvc.log",
-        env::temp_dir().to_string_lossy(),
-        // chrono::Local::now().format("%Y%m%d-%H%M%S")
-    );
+    let logfilename = &format!("{}/xvc.log", env::temp_dir().to_string_lossy(),);
 
     let logfile = Path::new(&logfilename);
 
@@ -38,8 +40,6 @@ fn init_logging(term_level: Option<LevelFilter>, file_level: Option<LevelFilter>
         out.finish(format_args!(
             "[{}][{}:{}] {}",
             record.level(),
-            // chrono::Local::now().format("[%H:%M:%S]"),
-            // record.target(),
             record.file().get_or_insert("None"),
             record.line().get_or_insert(0),
             message
@@ -74,10 +74,10 @@ fn init_logging(term_level: Option<LevelFilter>, file_level: Option<LevelFilter>
     };
 }
 
-// Used to differentiate between different types of outputs.
+/// Different channels of outputs Xvc can print
 #[derive(Clone, Debug)]
 pub enum XvcOutputLine {
-    /// The output that we should be reporting
+    /// The output that we should be reporting to user
     Output(String),
     /// For informational messages the user doesn't usually follow
     Info(String),
@@ -94,22 +94,28 @@ pub enum XvcOutputLine {
 }
 
 impl XvcOutputLine {
+    /// print [INFO] `s`
     pub fn info(s: &str) -> Self {
         Self::Info(s.to_string())
     }
-
+    /// print [WARN] `s`
     pub fn warn(s: &str) -> Self {
         Self::Warn(s.to_string())
     }
 
+    /// print [ERROR] `s`
     pub fn error(s: &str) -> Self {
         Self::Error(s.to_string())
     }
 
+    /// print [PANIC] `s`
+    ///
+    /// Does not panic. Developer should call `panic!` macro separately.
     pub fn panic(s: &str) -> Self {
         Self::Panic(s.to_string())
     }
 
+    /// Increment in progress bar
     pub fn tick(n: usize) -> Self {
         Self::Tick(n)
     }
