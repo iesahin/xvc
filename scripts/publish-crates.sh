5503b4637d8f3dd74a9f33f5160b02dfd08144b1
Restyled.io <commits@restyled.io>
2022-11-20 15:46:08 +0000

    Restyled by shfmt

--- scripts/publish-crates.sh
+++ scripts/publish-crates.sh
@@ -2,9 +2,8 @@
 
 set -vuex
 
-for dir in test_helper logging ecs walker config core pipeline storage file lib workflow_tests ; do
-    cd "$dir"
-    cargo publish
-    cd -
+for dir in test_helper logging ecs walker config core pipeline storage file lib workflow_tests; do
+  cd "$dir"
+  cargo publish
+  cd -
 done
-
