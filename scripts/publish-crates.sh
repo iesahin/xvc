ede3596856288f00d7e5444561ff7bccfa1e06e6
Restyled.io <commits@restyled.io>
2022-11-20 16:10:25 +0000

    Restyled by shfmt

--- scripts/publish-crates.sh
+++ scripts/publish-crates.sh
@@ -3,10 +3,8 @@
 # Adding -e to this mix causes script to fail when cargo publish finds an already published crate.
 set -vux
 
-
-for dir in test_helper logging ecs walker config core pipeline storage file lib workflow_tests ; do
-    cd "$dir"
-    cargo publish
-    cd -
+for dir in test_helper logging ecs walker config core pipeline storage file lib workflow_tests; do
+  cd "$dir"
+  cargo publish
+  cd -
 done
-
