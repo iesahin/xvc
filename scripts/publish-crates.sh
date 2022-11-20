e86da99b5094838825e78add5c4b4ea3f0a8a29b
Emre Sahin <github@emresult.com>
2022-11-20 19:03:29 +0300

    removed -e option from set

--- scripts/publish-crates.sh
+++ scripts/publish-crates.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
-set -vuex
+# Adding -e to this mix causes script to fail when cargo publish finds an already published crate.
+set -vux
+
 
 for dir in test_helper logging ecs walker config core pipeline storage file lib workflow_tests ; do
     cd ${dir}
