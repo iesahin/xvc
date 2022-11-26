45e29b3082b4b51a02576cb5ad42585219c6db4b
Emre Sahin <github@emresult.com>
2022-11-26 10:35:43 +0300

    updated workflow and publish script to run in bash

---
+++ book/src/ref/xvc-alias.md
@@ -0,0 +1,1 @@
+# xvc alias
--- scripts/publish-crates.sh
+++ scripts/publish-crates.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Adding -e to this mix causes script to fail when cargo publish finds an already published crate.
 set -vux
@@ -7,7 +7,7 @@ local base="$(dirname $0)/.."
 
 for dir in test_helper logging ecs walker config core pipeline storage file lib workflow_tests ; do
     cargo update
-    cd ${base}/${dir}
+    cd "${base}/${dir}"
     cargo publish
     cd -
 done
