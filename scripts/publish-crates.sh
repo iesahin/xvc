421fcbf14c02fec5e8cc22e7451224789d7039b0
Emre Sahin <github@emresult.com>
2022-11-27 17:44:33 +0300

    fix scripts

--- scripts/publish-crates.sh
+++ scripts/publish-crates.sh
@@ -3,7 +3,7 @@
 # Adding -e to this mix causes script to fail when cargo publish finds an already published crate.
 set -vux
 
-local base="$(dirname $0)/.."
+base="$(dirname $0)/.."
 
 for dir in test_helper logging ecs walker config core pipeline storage file lib workflow_tests ; do
     cargo update
