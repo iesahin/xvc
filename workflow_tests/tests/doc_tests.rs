83add2ee49a0224320c4c5557a4d9d39fb0dee2b
Emre Sahin <github@emresult.com>
2022-12-03 13:11:07 +0300

    better output message

--- workflow_tests/tests/doc_tests.rs
+++ workflow_tests/tests/doc_tests.rs
@@ -75,7 +75,7 @@ fn link_to_docs() -> Result<()> {
             let in_dir = test_collection_dir.join(&in_dir_name);
             let input_template_dir = template_dir_root.join(&in_dir_name);
             if input_template_dir.exists() {
-                println!("Copying template dir: {input_template_dir:?}");
+                println!("Copying template dir: {input_template_dir:?} to {in_dir:?}");
                 fs_extra::dir::copy(&input_template_dir, &in_dir, &CopyOptions::default())
                     .map_err(|e| anyhow!("FS Extra Error: {e:?}"))?;
             } else {
