f6c3520c859a6d04a0c94172ebff575ef6955b3d
Emre Sahin <github@emresult.com>
2022-12-03 13:22:05 +0300

    try to fix

--- workflow_tests/tests/doc_tests.rs
+++ workflow_tests/tests/doc_tests.rs
@@ -77,7 +77,6 @@ fn link_to_docs() -> Result<()> {
             let input_template_dir = cwd.join(template_dir_root.join(&in_dir_name));
             if input_template_dir.exists() {
                 println!("Copying template dir: {input_template_dir:?} to {in_dir:?}");
-                fs::create_dir(&in_dir)?;
                 fs_extra::dir::copy(&input_template_dir, &in_dir, &CopyOptions::default())
                     .map_err(|e| anyhow!("FS Extra Error: {e:?}"))?;
             } else {
