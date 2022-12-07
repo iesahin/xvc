2d9c62fb91ded41112a618cfbbbd9025de86ebc1
Emre Sahin <github@emresult.com>
2022-12-07 19:36:42 +0300

    no test_dispatch anymore

--- workflow_tests/src/lib.rs
+++ workflow_tests/src/lib.rs
@@ -1,9 +1,4 @@
-#![warn(missing_docs)]
 #![forbid(unsafe_code)]
-//! The main dispatching functions for the entire XVC CLI
-use std::path::Path;
-
-use config::XvcVerbosity;
 
 pub use xvc_config as config;
 pub use xvc_core as core;
@@ -14,20 +9,11 @@ pub use xvc_pipeline as pipeline;
 
 pub use xvc_logging::watch;
 
-use xvc::error::Result;
+use xvc::{cli::XvcCLI, error::Result};
 
 /// Run xvc in another process with `args` in `xvc_root_opt`.
 ///
 /// See [xvc::cli::test_dispatch].
-#[cfg(test)]
-pub fn test_dispatch(
-    xvc_root_opt: Option<&Path>,
-    args: Vec<&str>,
-    verbosity: XvcVerbosity,
-) -> Result<String> {
-    log::trace!("*********** TEST COMMAND ************");
-
-    watch!(args);
-
-    xvc::cli::test_dispatch(xvc_root_opt, &args, verbosity)
+pub fn dispatch(cli_opts: XvcCLI) -> Result<()> {
+    xvc::cli::dispatch(cli_opts)
 }
