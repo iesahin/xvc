777f122a8c78dd2d468e826c77084d455fdbe874
Emre Sahin <github@emresult.com>
2022-12-07 11:40:50 +0300

    simplified

--- workflow_tests/tests/test_core_aliases.rs
+++ workflow_tests/tests/test_core_aliases.rs
@@ -2,16 +2,10 @@ mod common;
 use common::*;
 use xvc_config::XvcVerbosity;
 
-use xvc::test_dispatch;
-
 use xvc::error::Result;
 
 #[test]
 fn test_aliases() -> Result<()> {
     test_logging(log::LevelFilter::Trace);
-    let out = test_dispatch(None, vec!["aliases"], XvcVerbosity::Trace)?;
-
-    assert!(out.len() > 0);
-
-    Ok(())
+    assert_xvc(&["aliases"], None, XvcVerbosity::Trace, |out| out.len() > 0)
 }
