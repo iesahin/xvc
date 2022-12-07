7bcfe8b4d47bcc24c294e3c14337edff84492d25
Emre Sahin <github@emresult.com>
2022-12-07 10:09:42 +0300

    explicit type

--- workflow_tests/tests/test_file_hash.rs
+++ workflow_tests/tests/test_file_hash.rs
@@ -23,7 +23,7 @@ fn create_directory_hierarchy() -> Result<XvcRoot> {
 fn test_file_hash() -> Result<()> {
     // setup::logging(LevelFilter::Trace);
     let xvc_root = create_directory_hierarchy()?;
-    let x = |cmd: &[&str]| {
+    let x = |cmd: &[&str]| -> Result<String> {
         let mut c = vec!["xvc", "file"];
         c.extend(cmd);
         xvc::test_dispatch(Some(&xvc_root), c, XvcVerbosity::Warn)
