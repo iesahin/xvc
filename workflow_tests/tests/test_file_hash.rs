454d54d4b6a38e8806df3559746f4123a62f870c
Emre Sahin <github@emresult.com>
2022-12-07 19:40:11 +0300

    updated tests

--- workflow_tests/tests/test_file_hash.rs
+++ workflow_tests/tests/test_file_hash.rs
@@ -23,37 +23,41 @@ fn create_directory_hierarchy() -> Result<XvcRoot> {
 fn test_file_hash() -> Result<()> {
     // setup::logging(LevelFilter::Trace);
     let xvc_root = create_directory_hierarchy()?;
-    let x = |cmd: &[&str]| -> Result<String> {
-        let mut c = vec!["xvc", "file"];
-        c.extend(cmd);
-        xvc::test_dispatch(Some(&xvc_root), c, XvcVerbosity::Warn)
-    };
+    let x = |cmd: &[&str]| common::run_xvc(Some(&xvc_root), cmd, XvcVerbosity::Trace);
 
     let re_match = |output, regex| {
         let regex = Regex::new(regex).unwrap();
-        assert!(regex.is_match(output), "output: {}", output);
+        regex.is_match(output)
     };
 
-    let dir_hash = x(&["hash", "dir-0001/"])?;
-    re_match(&dir_hash, ".*");
-    let line_count = dir_hash.lines().count();
-    watch!(line_count);
-    assert!(line_count == 100, "{}", line_count);
-    let images_hash = x(&["hash", "file-0000.bin"])?;
-    re_match(
+    let dir_hash = x(&["file", "hash", "dir-0001/"])?;
+    assert!({ dir_hash.lines().count() == 100 });
+    let images_hash = x(&["file", "hash", "file-0000.bin"])?;
+    assert!(re_match(
         &images_hash,
         "^a572622134fcb28679d2de66d225cc2a41c2594baa909781c0726eb7702baeb1\tfile-0000.bin.*",
-    );
-    let images_as_text_hash = x(&["hash", "--text-or-binary", "text", "file-0000.bin"])?;
-    assert!(images_hash == images_as_text_hash);
+    ));
+
+    let text_hash = x(&["file", "hash", "--text-or-binary", "text", "file-0000.bin"])?;
+    assert!(re_match(
+        &th,
+        "^a572622134fcb28679d2de66d225cc2a41c2594baa909781c0726eb7702baeb1\tfile-0000.bin.*"
+    ));
 
     let binary_hash = x(&[
+        "file",
         "hash",
         "--text-or-binary",
         "binary",
         "dir-0001/file-0010.bin",
     ])?;
-    let text_hash = x(&["hash", "--text-or-binary", "text", "dir-0001/file-0010.bin"])?;
+    let text_hash = x(&[
+        "file",
+        "hash",
+        "--text-or-binary",
+        "text",
+        "dir-0001/file-0010.bin",
+    ])?;
 
     assert!(
         binary_hash != text_hash,
