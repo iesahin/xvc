6f066067da6fdf673d3ac43cbad637e502f9233f
Emre Sahin <github@emresult.com>
2022-12-09 20:18:40 +0300

    added cleanup

--- workflow_tests/tests/test_core_types.rs
+++ workflow_tests/tests/test_core_types.rs
@@ -5,8 +5,8 @@ use proptest::prelude::*;
 
 use std::path::Path;
 use std::thread::sleep;
-use std::time;
 use std::time::Duration;
+use std::{fs, time};
 use xvc::core::types::*;
 use xvcfiletype::XvcFileType;
 use xvcmetadata::XvcMetadata;
@@ -19,7 +19,7 @@ fn test_xvc_file_metadata(filesize in 0..100000u64, filestr in "file-[0-9][0-9][
     test_logging(log::LevelFilter::Trace);
 
     // setup::logging(LevelFilter::Trace);
-    let _dir = common::run_in_temp_dir();
+    let dir = common::run_in_temp_dir();
 
     let filename = Path::new(&filestr);
     common::generate_random_file(&filename, filesize as usize);
@@ -33,6 +33,8 @@ fn test_xvc_file_metadata(filesize in 0..100000u64, filestr in "file-[0-9][0-9][
         .duration_since(xvc_md_1.modified.unwrap())
         .unwrap();
     prop_assert!(duration < Duration::from_secs(2));
+
+    fs::remove_dir_all(&dir).unwrap();
 }
 
 
