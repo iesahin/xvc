e1e3a8a9bd2027abd16e3ba9fcca0a1b12a556f3
Emre Sahin <github@emresult.com>
2022-12-09 10:03:20 +0300

    updated mdbook version

--- book/build-netlify.sh
+++ book/build-netlify.sh
@@ -3,5 +3,5 @@
 set -vuex
 
 curl -L https://github.com/badboy/mdbook-mermaid/releases/download/v0.12.3/mdbook-mermaid-v0.12.3-x86_64-unknown-linux-gnu.tar.gz | tar xvz
-curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.19/mdbook-v0.4.19-x86_64-unknown-linux-gnu.tar.gz | tar xvz
+curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.21/mdbook-v0.4.21-x86_64-unknown-linux-gnu.tar.gz | tar xvz
 PATH=${PATH}:${PWD} mdbook build
