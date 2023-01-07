b0295d31da268dc4b722f615a163d19fc1e9f884
Emre Sahin <github@emresult.com>
2023-01-07 12:58:48 +0300

    pretty print

--- workflow_tests/tests/common/mod.rs
+++ workflow_tests/tests/common/mod.rs
@@ -53,7 +53,11 @@ pub fn run_xvc(cwd: Option<&Path>, args: &[&str], verbosity: XvcVerbosity) -> Re
 
     assert!(output.status.success(), "Command failed: {:?}", prepared);
 
-    let output_str = format!("{:#?}", String::from_utf8(output.stdout)?);
+    let output_str = format!(
+        "Command: {prepared:#?}\nStdout: {}\nStderr: {}",
+        String::from_utf8(output.stdout)?,
+        String::from_utf8(output.stderr)?
+    );
     watch!(output_str);
     Ok(output_str)
 }
