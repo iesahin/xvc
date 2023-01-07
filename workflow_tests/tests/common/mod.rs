cf436b3ab31ee2a21bc32c2e247e727399854e90
Emre Sahin <github@emresult.com>
2023-01-07 13:04:38 +0300

    remove watch

--- workflow_tests/tests/common/mod.rs
+++ workflow_tests/tests/common/mod.rs
@@ -58,7 +58,6 @@ pub fn run_xvc(cwd: Option<&Path>, args: &[&str], verbosity: XvcVerbosity) -> Re
         String::from_utf8(output.stdout)?.replace("\\n", "\n"),
         String::from_utf8(output.stderr)?.replace("\\n", "\n"),
     );
-    watch!(output_str);
     Ok(output_str)
 }
 
