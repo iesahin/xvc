e511808798d660053d08f338c461465ebb1d1b61
Emre Sahin <github@emresult.com>
2023-01-11 06:46:47 +0300

    doesn't run with polling watcher

--- pipeline/src/pipeline/mod.rs
+++ pipeline/src/pipeline/mod.rs
@@ -243,7 +243,7 @@ pub fn the_grand_pipeline_loop(xvc_root: &XvcRoot, pipeline_name: String) -> Res
     let all_deps = xvc_root.load_r1nstore::<XvcStep, XvcDependency>()?;
     let all_outs = xvc_root.load_r1nstore::<XvcStep, XvcOutput>()?;
     let (mut pmm, ignore_rules) = all_paths_and_metadata(xvc_root);
-    let (_fs_watcher, fs_receiver) = make_polling_watcher(ignore_rules)?;
+    let (_fs_watcher, fs_receiver) = make_watcher(ignore_rules)?;
 
     let pipeline_len = pipeline_steps.len();
 
