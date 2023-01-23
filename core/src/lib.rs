8b82ae585dbbca8f09ecfce9ea2557a6012fef68
Emre Sahin <github@emresult.com>
2023-01-23 08:54:55 +0300

    updated default format string for xvc file list

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -94,16 +94,16 @@ pub fn default_project_config(use_git: bool) -> String {
     format!(
         r##"
 [core]
-# The repository id. Please do not delete or change it. 
-# This is used to identify the repository and generate paths in storages. 
-# In the future it may be used to in other ways. 
+# The repository id. Please do not delete or change it.
+# This is used to identify the repository and generate paths in storages.
+# In the future it may be used to in other ways.
 guid = "{guid}"
-# Default verbosity level. 
+# Default verbosity level.
 # One of "error", "warn", "info"
 verbosity = "error"
 
 [git]
-# Automate git operations. 
+# Automate git operations.
 # Turning this off leads Xvc to behave as if it's not in a Git repository.
 # Not recommended unless you're really not using Git
 use_git = {use_git}
@@ -113,25 +113,25 @@ use_git = {use_git}
 command = "git"
 
 # Commit changes in .xvc/ directory after commands.
-# You can set this to false if you want to commit manually. 
+# You can set this to false if you want to commit manually.
 auto_commit = true
 
 # Stage changes in .xvc/ directory without committing.
-# auto_commit implies auto_stage. 
-# If you want to commit manually but don't want to stage after individual Xvc commands, you can set this to true. 
+# auto_commit implies auto_stage.
+# If you want to commit manually but don't want to stage after individual Xvc commands, you can set this to true.
 auto_stage = false
 
 [cache]
 # The cache type for XVC. It may take copy, hardlink, symlink, reflink as values.
 # The default is copy to make sure the options is portable.
 # Copy duplicates the file content, while hardlink, symlink and reflink only create a new path to the file.
-# Note that hardlink and symlink are read-only as they link the files in cache. 
+# Note that hardlink and symlink are read-only as they link the files in cache.
 type = "copy"
-# The hash algorithm used for the cache. 
-# It may take blake3, blake2, sha2 or sha3 as values. 
+# The hash algorithm used for the cache.
+# It may take blake3, blake2, sha2 or sha3 as values.
 # All algorithms are selected to produce 256-bit hashes, so sha2 means SHA2-256, blake2 means BLAKE2s, etc.
-# The cache path is produced by prepending algorithm name to the cache. 
-# Blake3 files are in .xvc/b3/, while sha2 files are in .xvc/s2/ etc. 
+# The cache path is produced by prepending algorithm name to the cache.
+# Blake3 files are in .xvc/b3/, while sha2 files are in .xvc/s2/ etc.
 algorithm = "blake3"
 
 [file]
@@ -149,20 +149,20 @@ force = false
 # Auto check each file individually and treat it as text if it's text.
 text_or_binary = "auto"
 
-# Don't use parallelism in track operations. 
+# Don't use parallelism in track operations.
 # Note that some of the operations are implemented in parallel by default, and this option affects some heavier operations.
 no_parallel = false
 
 [file.list]
 
 # Format for `xvc file list` rows. You can reorder or remove columns.
-# The following are the keys for each row: 
+# The following are the keys for each row:
 # - {{acd64}}:  actual content digest. All 64 digits from the workspace file's content.
-# - {{acd8}}:  actual content digest. First 8 digits the file content digest. 
+# - {{acd8}}:  actual content digest. First 8 digits the file content digest.
 # - {{aft}}:  actual file type. Whether the entry is a file (F), directory (D),
-#   symlink (S), hardlink (H) or reflink (R). 
+#   symlink (S), hardlink (H) or reflink (R).
 # - {{asz}}:  actual size. The size of the workspace file in bytes. It uses MB,
-#   GB and TB to represent sizes larger than 1MB. 
+#   GB and TB to represent sizes larger than 1MB.
 # - {{ats}}:  actual timestamp. The timestamp of the workspace file.
 # - {{cst}}:  cache status. One of "=", ">", "<", "X", or "?" to show
 #   whether the file timestamp is the same as the cached timestamp, newer,
@@ -175,11 +175,11 @@ no_parallel = false
 # - {{rsz}}:  recorded size. The size of the cached content in bytes. It uses
 #   MB, GB and TB to represent sizes larged than 1MB.
 # - {{rts}}:  recorded timestamp. The timestamp of the cached content.
-# 
-# There are no escape sequences in the format string. 
+#
+# There are no escape sequences in the format string.
 # If you want to add a tab, type it to the string.
-# If you want to add a literal double curly brace, open an issue. 
-format = "{{{{aft}}}}{{{{rct}}}} {{{{asz}}}} {{{{ats}}}}   {{{{name}}}}  {{{{rcd8}}}} {{{{acd8}}}}"
+# If you want to add a literal double curly brace, open an issue.
+format = "{{{{aft}}}}{{{{rct}}}} {{{{asz}}}} {{{{ats}}}} {{{{rcd8}}}} {{{{acd8}}}} {{{{name}}}}"
 
 # Default sort order for `xvc file list`.
 # Valid values are
