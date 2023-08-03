8e5af625131eb6e21ff7bff47f9e806af7bcce70
Emre Sahin <github@emresult.com>
2023-08-03 21:04:52 +0300

    generic diff

--- pipeline/src/pipeline/deps/generic.rs
+++ pipeline/src/pipeline/deps/generic.rs
@@ -50,4 +50,31 @@ impl GenericDep {
 
 impl Diffable for GenericDep {
     type Item = GenericDep;
+
+    /// Compare the command itself, not the output.
+    fn diff_superficial(record: &Self::Item, actual: &Self::Item) -> Diff<Self::Item> {
+        if record.generic_command == actual.generic_command {
+            Diff::Identical
+        } else {
+            Diff::Different {
+                record: record.clone(),
+                actual: actual.clone(),
+            }
+        }
+    }
+
+    /// This is to calculate two entities with a thorough comparison.
+    /// e.g. content of a file, content of a URL etc.
+    /// You may need to update actual's content before calling this.
+    fn diff_thorough(record: &Self::Item, actual: &Self::Item) -> Diff<Self::Item> {
+        let actual = actual.clone().update_output_digest().unwrap();
+        if actual == *record {
+            Diff::Identical
+        } else {
+            Diff::Different {
+                record: record.clone(),
+                actual,
+            }
+        }
+    }
 }
