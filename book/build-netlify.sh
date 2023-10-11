887eec7adb90c614c144330f4c442a9df65cc656
Emre Sahin <github@emresult.com>
2023-10-11 23:38:00 +0300

    updated build-netlify script

--- book/build-netlify.sh
+++ book/build-netlify.sh
@@ -2,6 +2,8 @@
 
 set -vuex
 
-curl -L https://github.com/badboy/mdbook-mermaid/releases/download/v0.12.3/mdbook-mermaid-v0.12.3-x86_64-unknown-linux-gnu.tar.gz | tar xvz
-curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.21/mdbook-v0.4.21-x86_64-unknown-linux-gnu.tar.gz | tar xvz
+curl -L https://github.com/tommilligan/mdbook-admonish/releases/download/v1.13.0/mdbook-admonish-v1.13.0-x86_64-unknown-linux-gnu.tar.gz | tar xvz
+curl -L https://github.com/badboy/mdbook-mermaid/releases/download/v0.12.6/mdbook-mermaid-v0.12.6-x86_64-unknown-linux-gnu.tar.gz | tar xvz
+curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.35/mdbook-v0.4.35-x86_64-unknown-linux-gnu.tar.gz | tar xvz
+
 PATH=${PATH}:${PWD} mdbook build
