983459fcdd9df0a230640981e639b87e39e6b110
Emre Sahin <github@emresult.com>
2023-11-18 01:42:53 +0300

    update script

--- workflow_tests/templates/create-a-data-pipeline.in/create-subsets.zsh
+++ workflow_tests/templates/create-a-data-pipeline.in/create-subsets.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-for p in {1..60} ; do xvc file copy data/data/input_${p}_* data/train/ ; done
+for p in {1..60} ; do xvc file copy 'data/data/input_${p}_*' data/train/ ; done
 
-for p in {61..80} ; do xvc file copy data/data/input_${p}_* data/validate/ ; done
+for p in {61..80} ; do xvc file copy 'data/data/input_${p}_*' data/validate/ ; done
 
-for p in {81..100} ; do xvc file copy data/data/input_${p}_* data/test/ ; done
+for p in {81..100} ; do xvc file copy 'data/data/input_${p}_*' data/test/ ; done
