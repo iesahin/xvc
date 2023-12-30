a5d7885df9768d8c585c741bcc676ce2d619d00f
Emre Sahin <github@emresult.com>
2023-12-30 20:14:29 +0300

    add new test for pmp

---
+++ workflow_tests/tests/test_core_util_file_pmp.rs
@@ -0,0 +1,68 @@
+mod common;
+use common::*;
+use fs_extra::dir::create;
+use log::LevelFilter;
+use xvc_core::{
+    util::xvcignore::{walk_parallel, walk_serial},
+    XvcPath, XvcPathMetadataProvider,
+};
+
+use std::{fs::remove_file, path::Path, time::Duration};
+
+use xvc_logging::watch;
+
+use xvc::error::Result;
+
+#[test]
+fn test_pmp() -> Result<()> {
+    test_logging(LevelFilter::Trace);
+    let (output_sender, output_receiver) = crossbeam_channel::unbounded();
+    let xvc_root = run_in_temp_xvc_dir()?;
+    watch!(xvc_root);
+
+    // We create this directory tree BEFORE the pmp so that we'll assert that it won't be notified
+    // and won't collect these.
+    let n_dirs = 3;
+    let n_files = 2;
+    create_directory_tree(&xvc_root, n_dirs, n_files, 10, None)?;
+
+    let pmp = XvcPathMetadataProvider::new(&output_sender, &xvc_root)?;
+
+    // Test skip list
+    for skipped in [".dvc", ".xvc", ".git"] {
+        let xp = XvcPath::new(&xvc_root, xvc_root.absolute_path(), Path::new(skipped))?;
+        assert!(!pmp.path_present(&xp), "Result Contains {:?}", xp)
+    }
+
+    // Test create / update / delete in the background
+    let fn1 = "file1.bin";
+    let path1 = xvc_root.absolute_path().join(fn1);
+    let xpath1 = XvcPath::new(&xvc_root, &xvc_root, &path1)?;
+    let orig_size = 100;
+    generate_random_file(&path1, orig_size, None);
+    let md1 = pmp.get(&xpath1);
+    assert!(md1.is_some());
+    assert!(md1.unwrap().is_file());
+    assert!(md1.unwrap().size == Some(orig_size as u64));
+
+    let new_size = 200;
+    generate_random_file(&path1, new_size, None);
+    let md1 = pmp.get(&xpath1);
+    assert!(md1.is_some());
+    assert!(md1.unwrap().is_file());
+    assert!(md1.unwrap().size == Some(new_size as u64));
+
+    remove_file(&path1)?;
+
+    assert!(!pmp.path_present(&xpath1));
+
+    let glob_paths = pmp.glob_paths("**/*.bin")?;
+    assert!(glob_paths.len() == n_dirs * n_files);
+
+    remove_file(Path::new("dir-0001/file-0001.bin"))?;
+
+    let glob_paths = pmp.glob_paths("**/*.bin")?;
+    assert!(glob_paths.len() == n_dirs * n_files - 1);
+
+    clean_up(&xvc_root)
+}
