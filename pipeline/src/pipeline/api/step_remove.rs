f5b4f1720c1d7bdce8c487140f76a5f55e6e0f46
Emre Sahin <github@emresult.com>
2024-01-10 20:33:17 +0300

    fix step dep bug

--- pipeline/src/pipeline/api/step_remove.rs
+++ pipeline/src/pipeline/api/step_remove.rs
@@ -14,25 +14,45 @@ pub fn cmd_step_remove(
     pipeline_name: &str,
     step_name: String,
 ) -> Result<()> {
-    let (pipeline_e, pipeline) = XvcPipeline::from_name(xvc_root, pipeline_name)?;
+    let (pipeline_e, _) = XvcPipeline::from_name(xvc_root, pipeline_name)?;
     let (step_e, step) = XvcStep::from_name(xvc_root, &pipeline_e, &step_name)?;
+
+    // We'll need this while removing dependencies
+    let pipeline_steps = xvc_root
+        .load_r1nstore::<XvcPipeline, XvcStep>()?
+        .children_of(&pipeline_e)?;
 
     // Remove dependencies
     xvc_root.with_r1nstore_mut::<XvcStep, XvcDependency>(
         |rs: &mut R1NStore<XvcStep, XvcDependency>| {
             let deps = rs.children_of(&step_e)?;
             for (dep_e, dep) in deps.iter() {
-                info!(output_snd, "Removing dep: {}", dep_e);
+                info!(output_snd, "Removing dep: {}", dep);
                 rs.remove_child(*dep_e)?;
+            }
+
+            // Remove any dependencies on this step
+            for (pipeline_step_e, pipeline_step) in pipeline_steps.iter() {
+                let deps = rs.children_of(pipeline_step_e)?;
+                for (dep_e, dep) in deps.iter() {
+                    if matches!(dep, XvcDependency::Step ( dep ) if dep.name == step.name) {
+                        info!(
+                            output_snd,
+                            "Removing step dep {} from {}", dep, pipeline_step
+                        );
+                        rs.remove_child(*dep_e)?;
+                    }
+                }
             }
             Ok(())
         },
     )?;
+
     // Remove outputs
     xvc_root.with_r1nstore_mut::<XvcStep, XvcOutput>(|rs: &mut R1NStore<XvcStep, XvcOutput>| {
         let outputs = rs.children_of(&step_e)?;
         for (output_e, output) in outputs.iter() {
-            info!(output_snd, "Removing output: {}", output_e);
+            info!(output_snd, "Removing output: {}", output);
             rs.remove_child(*output_e)?;
         }
         Ok(())
