cfbfe3e7c7d9c9d9b8a1de1be38f166934411cbf
Emre Sahin <github@emresult.com>
2024-08-30 14:01:32 +0300

    test

--- run-tests.zsh
+++ run-tests.zsh
@@ -1,1 +1,1 @@
-cargo test --features=test-minio -p xvc  --test test_storage_new_minio # -p xvc-core --lib -p xvc-walker --lib -p xvc-file --doc
+cargo test --features=test-minio -p xvc-core --lib # -p xvc-walker --lib -p xvc-file --doc
