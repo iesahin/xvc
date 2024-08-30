d26def3f1c7c5f43d481f105eaeb24348f88923f
Emre Sahin <github@emresult.com>
2024-08-30 13:45:13 +0300

    test

--- run-tests.zsh
+++ run-tests.zsh
@@ -1,1 +1,1 @@
-cargo test -p xvc  --test test_pipeline_dag # -p xvc --test test_pipeline_run -p xvc --test test_storage_new_minio -p xvc-core --lib -p xvc-walker --lib -p xvc-file --doc
+cargo test -p xvc --test test_pipeline_run  # -p xvc --test test_storage_new_minio -p xvc-core --lib -p xvc-walker --lib -p xvc-file --doc
