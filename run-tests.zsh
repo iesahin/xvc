5c7dbe7c634eed096cb44cb7ed8a53b2f82d4023
Emre Sahin <github@emresult.com>
2024-11-29 22:53:32 +0300

    update dev test

--- run-tests.zsh
+++ run-tests.zsh
@@ -1,3 +1,3 @@
-XVC_TRYCMD_TESTS=storage,file,pipeline,core,start TRYCMD=overwrite cargo test -p xvc --test z_test_docs
+XVC_TRYCMD_TESTS=storage,file,pipeline,core,start TRYCMD=overwrite cargo test -p xvc --test test_file_list
 
 # cargo test --features test-ci -p xvc --test test_storage_new_minio
