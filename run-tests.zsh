ba12b242bb768f3440f265d35578554992137f3e
Emre Sahin <github@emresult.com>
2024-11-30 17:10:07 +0300

    update run-tests to have similar env vars

--- run-tests.zsh
+++ run-tests.zsh
@@ -1,3 +1,3 @@
-XVC_TRYCMD_TESTS=storage,file,pipeline,core,start TRYCMD=overwrite cargo test 
+LLVM_PROFILE_FILE="${TMPDIR}/xvc-%p-%m.profraw" CARGO_INCREMENTAL=0 RUSTFLAGS="-Cinstrument-coverage" XVC_TRYCMD_TESTS=storage,file,pipeline,core,start TRYCMD=overwrite cargo test --features test-ci -p xvc --test test_file_list
 
 # cargo test --features test-ci -p xvc --test test_storage_new_minio
