43c3c5809355547fc593b0a41101a3561090facb
Emre Sahin <contact@emresahin.net>
2025-12-20 21:26:27 +0300

    Update run-tests not to run

--- run-tests.zsh
+++ run-tests.zsh
@@ -1,6 +1,6 @@
 # LLVM_PROFILE_FILE="${TMPDIR}/xvc-%p-%m.profraw" CARGO_INCREMENTAL=0 RUSTFLAGS="-Cinstrument-coverage" XVC_TRYCMD_TESTS=storage,file,pipeline,core,start TRYCMD=overwrite cargo llvm-cov --features test-ci --lcov --output-path lcov.info -p xvc # --test z_test_docs
 # rws is for local dev, run-with-secrets script
-XVC_TRYCMD_TESTS=storage,file,pipeline,core,start TRYCMD=overwrite rws cargo test --features test-ci -p xvc
+# XVC_TRYCMD_TESTS=storage,file,pipeline,core,start TRYCMD=overwrite rws cargo test --features test-ci -p xvc
 
 # XVC_TRYCMD_TESTS=storage,file,pipeline,core,start TRYCMD=overwrite cargo test --features test-ci -p xvc --test test_storage_new_rclone
 
