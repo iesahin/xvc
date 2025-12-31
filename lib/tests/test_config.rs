81fd25ace68f07c6f2a9a3ab073ed32b2ec10ad8
Emre Sahin <contact@emresahin.net>
2026-01-01 00:43:05 +0300

    Happy new year Xvc! May your users double, triple, quaruple this year!

--- lib/tests/test_config.rs
+++ lib/tests/test_config.rs
@@ -165,13 +165,10 @@ fn test_xvc_config_new_v2_precedence() -> Result<()> {
     // 1. Project config file
     let project_config_path = temp_dir.join("xvc.toml");
     let project_toml = r#"
-#;
-
 [core]
-verbosity = "info" // Project
+verbosity = "info" # Project
 [git]
-use_git = false // Project
-#
+use_git = false # Project
 "#;
     fs::write(&project_config_path, project_toml)?;
 
@@ -180,7 +177,7 @@ use_git = false // Project
     let local_toml = r#"#;
 
 [core]
-verbosity = "debug" // Local overrides project
+verbosity = "debug" # Local overrides project
 #
 "#;
     fs::write(&local_config_path, local_toml)?;
@@ -224,7 +221,9 @@ fn test_find_value_source() -> Result<()> {
     let project_config_path = temp_dir.join("xvc.toml");
     fs::write(&project_config_path, "[git]\nuse_git = false")?;
 
-    std::env::set_var("XVC_CACHE.ALGORITHM", "sha256");
+    // This seems to leak from other tests
+    std::env::remove_var("XVC_GIT.USE_GIT");
+    std::env::set_var("XVC_FILE.RECHECK.METHOD", "reflink");
 
     let params = XvcLoadParams::new(current_dir.clone(), Some(current_dir))
         .include_system_config(false)
@@ -242,7 +241,7 @@ fn test_find_value_source() -> Result<()> {
     );
 
     assert_eq!(
-        config_loader.find_value_source("cache.algorithm"),
+        config_loader.find_value_source("file.recheck.method"),
         Some(XvcConfigOptionSource::Environment)
     );
 
@@ -258,7 +257,7 @@ fn test_find_value_source() -> Result<()> {
 
     assert_eq!(config_loader.find_value_source("invalid.key"), None);
 
-    std::env::remove_var("XVC_CACHE.ALGORITHM");
+    std::env::remove_var("XVC_FILE.RECHECK.METHOD");
     fs::remove_dir_all(temp_dir)?;
     Ok(())
 }
